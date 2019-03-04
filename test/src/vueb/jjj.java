b53e5fd9f9303c868642f2e8471d7e0e4e3e8e47
vandong6585 <vandong6585@gmail.com>
2019-03-04 10:35:28 +0700

    ee

--- test/src/vueb/jjj.java
+++ test/src/vueb/jjj.java
@@ -10,4 +10,9 @@ public class jjj {
 		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		  driver.manage().window().maximize();
 		  driver.get("http://demo.guru99.com/v4/");
+		  driver = new ChromeDriver();
+		  wait = new WebDriverWait (driver, 30);
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		  driver.manage().window().maximize();
+		  driver.get("http://demo.guru99.com/v4/");
 		  }
